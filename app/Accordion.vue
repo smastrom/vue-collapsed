ef77ecb8639363271861adf5b63969b21754bae0
smastrom <78644132+smastrom@users.noreply.github.com>
2022-11-19 17:31:31 +0100

    edit readme, added app

---
+++ app/Accordion.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { Collapse } from '../src/Collapse';
+import ExampleHeader from './ExampleHeader.vue';
+import fakeData from './fakeData.json';
+
+const questions = reactive(
+	fakeData.map(({ title, answer }, index) => ({ title, answer, isExpanded: index === 2 || false }))
+);
+
+function handleAccordion(selectedIndex: number) {
+	questions.forEach((_, index) => {
+		questions[index].isExpanded = index === selectedIndex ? !questions[index].isExpanded : false;
+	});
+}
+</script>
+
+<template>
+	<section class="Wrapper">
+		<ExampleHeader title="Accordion" href="" hint="Using reactive()" />
+		<div v-for="(question, index) in questions" :key="question.title" class="Section">
+			<button
+				:class="[
+					'Panel',
+					{
+						Active: question.isExpanded,
+					},
+				]"
+				@click="() => handleAccordion(index)"
+			>
+				{{ question.title }}
+			</button>
+			<Collapse as="section" :when="question.isExpanded" class="Collapse">
+				<p>
+					{{ question.answer }}
+				</p>
+			</Collapse>
+		</div>
+	</section>
+</template>
+
+<!-- Find styles in App.vue -->
---
+++ app/ExampleHeader.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{ title: string; href: string; hint?: string }>();
+</script>
+
+<template>
+	<header class="Header">
+		<div class="HeaderTitle">
+			<h2>{{ title }}</h2>
+			<p v-if="hint">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="17.5"
+					height="17.5"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<circle cx="12" cy="12" r="10"></circle>
+					<line x1="12" y1="16" x2="12" y2="12"></line>
+					<line x1="12" y1="8" x2="12.01" y2="8"></line></svg
+				>{{ hint }}
+			</p>
+		</div>
+		<a :href="`https://github.com/smastrom/${href}`" class="CodeLink" target="_blank">View Code</a>
+	</header>
+</template>
