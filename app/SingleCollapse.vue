a79ee5835dbb5f7ab84039c1f24ec8ce69802be2
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2022-11-20 09:55:48 +0100

    add license, edit pkg

--- app/SingleCollapse.vue
+++ app/SingleCollapse.vue
@@ -18,16 +18,10 @@ const collapseAttrs = {
 	id: 'my-collapse-id',
 	role: 'region',
 };
-
-const count = ref(0);
-
-function addCount() {
-	count.value++;
-}
 </script>
 
 <template>
-	<section class="Wrapper">
+	<article class="Wrapper">
 		<ExampleHeader title="Single Collapse" href="blob/master/app/SingleCollapse.vue" />
 		<div class="Section">
 			<button
@@ -40,15 +34,9 @@ function addCount() {
 				v-bind="toggleAttrs"
 				@click="handleCollapse"
 			>
-				Hello buddy, how are you today? {{ count }}
+				Hello buddy, how are you today?
 			</button>
-			<Collapse
-				v-bind="collapseAttrs"
-				as="section"
-				:when="isOpen"
-				class="Collapse"
-				:onExpanded="addCount"
-			>
+			<Collapse v-bind="collapseAttrs" as="section" :when="isOpen" class="Collapse">
 				<p>
 					As an interesting side note, as a head without a body, I envy the dead. Kids don't turn
 					rotten just from watching TV. Bender, I didn't know you liked cooking. That's so cute.
@@ -58,7 +46,7 @@ function addCount() {
 				</p>
 			</Collapse>
 		</div>
-	</section>
+	</article>
 </template>
 
 <!-- Find styles in App.vue -->
