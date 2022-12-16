87dd0fc3a153e8a8b0324a3046eaeec062b5f9d7
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2022-12-16 17:40:54 +0100

    add baseHeight, onStart callbacks, data attr

--- src/useColllapse.ts
+++ src/useColllapse.ts
@@ -1,85 +1,120 @@
-import { ref, watch, type Ref, type ComputedRef, type CSSProperties as CSS } from 'vue';
+import { ref, watch, computed, type Ref, type ComputedRef, type CSSProperties as CSS } from 'vue';
 
 type UseCollapseOptions = {
-	isExpanded: ComputedRef<boolean>;
+	isExpanded: ComputedRef<boolean> | Ref<boolean>;
+	baseHeight?: ComputedRef<number> | Ref<number>;
+	onExpand?: () => void;
 	onExpanded?: () => void;
+	onCollapse?: () => void;
 	onCollapsed?: () => void;
 };
 
+type TransitionState = 'expanding' | 'expanded' | 'collapsing' | 'collapsed';
+
 type UseCollapseReturn = {
 	style: Ref<CSS>;
+	state: Ref<TransitionState>;
 	onTransitionEnd: (event: TransitionEvent) => void;
 };
 
-const nextFrame = typeof window !== 'undefined' ? requestAnimationFrame : () => {};
-
 const fixedStyles: CSS = { padding: 0, border: 0, margin: 0 };
-const collapsedStyles: CSS = { display: 'none', ...fixedStyles };
 const performanceStyles: CSS = {
 	willChange: 'height',
-};
-const hiddenStyles: CSS = {
-	overflow: 'hidden',
-	height: 0,
 };
 
 export function useCollapse(
 	collapseRef: Ref<HTMLElement | null>,
-	{ isExpanded, onExpanded = () => {}, onCollapsed = () => {} }: UseCollapseOptions
+	{
+		isExpanded,
+		baseHeight = ref(0),
+		onExpand = () => {},
+		onExpanded = () => {},
+		onCollapse = () => {},
+		onCollapsed = () => {},
+	}: UseCollapseOptions
 ): UseCollapseReturn {
-	const style = ref<CSS>(!isExpanded.value ? collapsedStyles : fixedStyles);
+	const collapseStyles = computed<Record<'visible' | 'collapsed', CSS>>(() => {
+		const visible = {
+			overflow: 'hidden',
+			height: `${baseHeight.value}px`,
+		};
+		return {
+			visible,
+			collapsed: {
+				...fixedStyles,
+				...(baseHeight.value === 0 ? { display: 'none' } : visible),
+			},
+		};
+	});
+
+	const style = ref<CSS>(isExpanded.value ? fixedStyles : collapseStyles.value.collapsed);
+	const state = ref<TransitionState>(isExpanded.value ? 'expanded' : 'collapsed');
 
 	watch(
 		() => isExpanded.value,
 		(isExpanded) => {
-			if (collapseRef.value) {
-				requestAnimationFrame(() => {
-					if (isExpanded) {
+			requestAnimationFrame(() => {
+				if (isExpanded) {
+					/**
+					 * At this point CSS height is set to 'auto' and display to 'none'.
+					 * In order to get the scrollHeight to trigger the transition,
+					 * we get rid of display: none by replacing the styles.
+					 *
+					 * We set the height to baseHeight as it will be the property we will
+					 * transition from and also to avoid the element being visible for
+					 * the duration of this frame in case baseHeight equals to 0.
+					 */
+					style.value = {
+						...fixedStyles,
+						...performanceStyles,
+						...collapseStyles.value.visible,
+					};
+					requestAnimationFrame(() => {
 						/**
-						 * At this point CSS height is set to 'auto' and display to 'none'.
-						 * In order to get the scrollHeight to trigger the transition,
-						 * we get rid of display: none by replacing the styles.
-						 *
-						 * We set the height to zero as it will be the property we will
-						 * transition from and also to avoid the element being visible for
-						 * the duration of this frame.
-						 */
-						style.value = {
-							...fixedStyles,
-							...performanceStyles,
-							...hiddenStyles,
-						};
-						nextFrame(() => {
-							/**
-							 * Set the height to scrollHeight and trigger the transition.
-							 */
-							style.value = {
-								...style.value,
-								...getHeightStyles(collapseRef.value?.scrollHeight),
-							};
-						});
-					} else {
-						/**
-						 * At this point CSS height property is set to 'auto'.
-						 * Since the element is visible we get the scrollHeight
-						 * and set it as height.
+						 * Set the height to scrollHeight and trigger the transition.
 						 */
 						style.value = {
 							...style.value,
-							...performanceStyles,
-							...getHeightStyles(collapseRef.value?.scrollHeight),
+							...getHeightStyles(collapseRef.value?.scrollHeight, baseHeight.value),
 						};
-						nextFrame(() => {
-							/**
-							 * Set the height to 0 and trigger the transition.
-							 */
-							style.value = {
-								...style.value,
-								...hiddenStyles,
-							};
-						});
-					}
-				});
+						state.value = 'expanding';
+						onExpand();
+					});
+				} else {
+					/**
+					 * At this point CSS height property is set to 'auto'.
+					 * Since the element is visible we get the scrollHeight
+					 * and set it as height.
+					 */
+					style.value = {
+						...style.value,
+						...performanceStyles,
+						...getHeightStyles(collapseRef.value?.scrollHeight, baseHeight.value),
+					};
+					requestAnimationFrame(() => {
+						/**
+						 * Set the height to baseHeight and trigger the transition.
+						 */
+						style.value = {
+							...style.value,
+							...collapseStyles.value.visible,
+						};
+						state.value = 'collapsing';
+						onCollapse();
+					});
+				}
+			});
+		}
+	);
+
+	watch(
+		() => baseHeight.value,
+		(newbaseHeight) => {
+			if (!isExpanded.value) {
+				style.value = {
+					...style.value,
+					height: `${newbaseHeight}px`,
+				};
 			}
 		}
 	);
@@ -96,11 +131,13 @@ export function useCollapse(
 					collapseRef.value?.scrollHeight === parseFloat((event.target as HTMLElement).style.height)
 				) {
 					style.value = fixedStyles;
+					state.value = 'expanded';
 					onExpanded();
 				}
 			} else {
-				if (collapseRef.value?.style.height === '0px') {
-					style.value = collapsedStyles;
+				if (collapseRef.value?.style.height === `${baseHeight.value}px`) {
+					style.value = collapseStyles.value.collapsed;
+					state.value = 'collapsed';
 					onCollapsed();
 				}
 			}
@@ -109,13 +146,14 @@ export function useCollapse(
 
 	return {
 		style,
+		state,
 		onTransitionEnd,
 	};
 }
 
-function getHeightStyles(height = 0) {
+function getHeightStyles(height = 0, baseHeight = 0) {
 	return {
-		'--vc-auto-duration': `${getAutoDuration(height)}ms`,
+		'--vc-auto-duration': `${getAutoDuration(height - baseHeight)}ms`,
 		height: `${height}px`,
 	};
 }
