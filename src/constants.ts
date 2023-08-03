423d6ad9fe23011f2cea4f320d24543f29093347
smastrom <60471784+smastrom@users.noreply.github.com>
2023-08-04 01:23:07 +0200

    streamline auto duration handling and raf watcher calls

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,16 @@ export const AUTO_DUR_VAR = '--vc-auto-duration'
 
 export const DEFAULT_TRANSITION = `height var(${AUTO_DUR_VAR}) cubic-bezier(0.33, 1, 0.68, 1)`
 
-export const SAFE_STYLES = { padding: 0, border: 0, margin: 0 } as const
+export const SAFE_STYLES = { padding: 0 } as const
+
+export const VISUALLY_HIDDEN = {
+   position: 'absolute',
+   width: '1px',
+   height: '1px',
+   padding: '0',
+   margin: '-1px',
+   overflow: 'hidden',
+   clip: 'rect(0, 0, 0, 0)',
+   whiteSpace: 'nowrap',
+   border: '0',
+} as const
