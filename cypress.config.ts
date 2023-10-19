f0486cfebda627ad99cb9d78ab6e822a3a27d9c3
smastrom <smastrom@proton.me>
2023-10-19 19:01:54 +0200

    Tests - Drop experimental webkit tests, streamline actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
    component: {
       video: false,
-      experimentalWebKitSupport: true,
       devServer: {
          framework: 'vue',
          bundler: 'vite',
