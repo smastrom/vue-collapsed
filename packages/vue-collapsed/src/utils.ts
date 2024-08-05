747500d325773a16e9922d04a843a7cda4da4a93
smastrom <smastrom@proton.me>
2024-08-05 17:45:41 +0200

    Fix #31 - Update default transition value since Chromium 128

--- packages/vue-collapsed/src/utils.ts
+++ packages/vue-collapsed/src/utils.ts
@@ -4,8 +4,6 @@ import type { Ref } from 'vue'
 
 type RefEl = Ref<HTMLElement | null>
 
-const isFirefox = () => typeof navigator !== 'undefined' && navigator.userAgent.includes('Firefox')
-
 export function getComputedHeight(el: RefEl) {
    if (!el.value) return 0
    return parseFloat(getComputedStyle(el.value).height)
@@ -17,13 +15,10 @@ export function getTransitionProp(el: RefEl) {
    const { transition } = getComputedStyle(el.value)
 
    // If transition is not defined via CSS, return the default one referencing the auto duration
-   if (
-      transition === 'all 0s ease 0s' ||
-      (isFirefox() &&
-         transition ===
-            'all') /* Since Firefox v124, Gecko returns transition 'all' instead of 'all 0s ease 0s' */
-   )
+   if (transition === 'all 0s ease 0s' || transition === 'all') {
+      /* Since Firefox v124 and Chromium v128, their rendering engine transition computes 'all' instead of 'all 0s ease 0s'  */
       return { transition: DEFAULT_TRANSITION }
+   }
 
    return { transition }
 }
